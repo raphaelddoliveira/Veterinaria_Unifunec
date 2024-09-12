c0f3008c9f5d59aa8bda0d3920c20ff64761a5e0
raphaelddoliveira <raphaeldantasdeoliveira42@gmail.com>
2024-09-11 21:58:40 -0300

    Teste

--- view/Frmbairro.Designer.cs
+++ view/Frmbairro.Designer.cs
@@ -229,7 +229,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(16, 48);
-            this.label1.Name = "label1";
+            this.label1.Name = "label1" ;
             this.label1.Size = new System.Drawing.Size(43, 13);
             this.label1.TabIndex = 7;
             this.label1.Text = "Código:";
