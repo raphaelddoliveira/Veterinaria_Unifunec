8d154f0978223d9b0c2f93274506c8161c990b5a
123ThaliMelo <thalitaanmelo@hotmail.com>
2024-09-11 22:17:09 -0300

    Update Form1.Designer.cs
    Adicionei a Label no Form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.tipoAnimalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.endereçoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bairroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,8 +46,8 @@
             this.endereçoToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -56,21 +57,21 @@
             this.raçaToolStripMenuItem,
             this.tipoAnimalToolStripMenuItem});
             this.animalToolStripMenuItem.Name = "animalToolStripMenuItem";
-            this.animalToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.animalToolStripMenuItem.Size = new System.Drawing.Size(70, 24);
             this.animalToolStripMenuItem.Text = "Animal";
             this.animalToolStripMenuItem.Click += new System.EventHandler(this.animalToolStripMenuItem_Click);
             // 
             // raçaToolStripMenuItem
             // 
             this.raçaToolStripMenuItem.Name = "raçaToolStripMenuItem";
-            this.raçaToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.raçaToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
             this.raçaToolStripMenuItem.Text = "Raça";
             this.raçaToolStripMenuItem.Click += new System.EventHandler(this.raçaToolStripMenuItem_Click);
             // 
             // tipoAnimalToolStripMenuItem
             // 
             this.tipoAnimalToolStripMenuItem.Name = "tipoAnimalToolStripMenuItem";
-            this.tipoAnimalToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.tipoAnimalToolStripMenuItem.Size = new System.Drawing.Size(173, 26);
             this.tipoAnimalToolStripMenuItem.Text = "Tipo Animal";
             this.tipoAnimalToolStripMenuItem.Click += new System.EventHandler(this.tipoAnimalToolStripMenuItem_Click);
             // 
@@ -79,23 +80,34 @@
             this.endereçoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.bairroToolStripMenuItem});
             this.endereçoToolStripMenuItem.Name = "endereçoToolStripMenuItem";
-            this.endereçoToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.endereçoToolStripMenuItem.Size = new System.Drawing.Size(85, 24);
             this.endereçoToolStripMenuItem.Text = "Endereço";
             // 
             // bairroToolStripMenuItem
             // 
             this.bairroToolStripMenuItem.Name = "bairroToolStripMenuItem";
-            this.bairroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bairroToolStripMenuItem.Size = new System.Drawing.Size(132, 26);
             this.bairroToolStripMenuItem.Text = "Bairro";
             this.bairroToolStripMenuItem.Click += new System.EventHandler(this.bairroToolStripMenuItem_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(505, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
@@ -113,6 +125,7 @@
         private System.Windows.Forms.ToolStripMenuItem tipoAnimalToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem endereçoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bairroToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
     }
 }
 
